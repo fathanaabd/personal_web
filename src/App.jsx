aa6d554faa35e6a101d4750871a053cff5e6d169
Fathan Akbar A <fathan.aa.01@gmail.com>
2023-07-18 23:31:00 +0700

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-function App() {
-  const [count, setCount] = useState(0)
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCOeV1WCp-2rKyy5snT1Dnp48cuNNCM0W0",
+  authDomain: "personalweb-8b9f0.firebaseapp.com",
+  projectId: "personalweb-8b9f0",
+  storageBucket: "personalweb-8b9f0.appspot.com",
+  messagingSenderId: "178921788338",
+  appId: "1:178921788338:web:937873e8912d8fa895caab",
+  measurementId: "G-2RV99QX0XP",
+};
 
+export default function App() {
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="text-3xl font-bold">React starter</h1>
+      <h3 className="text-2xl font-light">
+        Vite, React, TailwindCSS, Prettier + ESLint, Firebase
+      </h3>
     </>
-  )
+  );
 }
-
-export default App
