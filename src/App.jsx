97530350086a8b66525d539dcbb30175b7192e76
Fathan Akbar A <fathan.aa.01@gmail.com>
2023-07-19 07:31:33 +0700

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getDatabase } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,17 +15,20 @@ const firebaseConfig = {
   messagingSenderId: "178921788338",
   appId: "1:178921788338:web:937873e8912d8fa895caab",
   measurementId: "G-2RV99QX0XP",
+  databaseURL: "https://DATABASE_NAME.firebaseio.com",
 };
 
 export default function App() {
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
+  // Initialize Realtime Database and get a reference to the service
+  const database = getDatabase(app);
   const analytics = getAnalytics(app);
   return (
     <>
       <h1 className="text-3xl font-bold">React starter</h1>
       <h3 className="text-2xl font-light">
-        Vite, React, TailwindCSS, Prettier + ESLint, Firebase
+        Vite, React, TailwindCSS, Prettier + ESLint, Firebase Hosting+RealtimeDatabase
       </h3>
     </>
   );
