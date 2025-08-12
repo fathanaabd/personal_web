fe4a24578def8ab746ea73ffd1e05233d63831d8
Fathan Akbar A <fathan.aa.01@gmail.com>
2025-08-12 23:51:52 +0700

    Test
    Test

--- src/components/ImAn.jsx
+++ src/components/ImAn.jsx
@@ -8,7 +8,7 @@ export default function ImAn() {
               im a
             </h1>
             <h1 className="text-3xl font-light leading-[38.80px] font-gelgooR400">
-              back-end engineer
+              software developer
             </h1>
           </div>
         </div>
--- src/components/SkillsHero.jsx
+++ src/components/SkillsHero.jsx
@@ -1,26 +1,26 @@
 export default function SkillsHero() {
   return (
     <>
-      <div className="container mx-auto py-12 px-6">
+      <div className="container mx-auto py-24 px-6">
         <div className="flex flex-col gap-2 text-white">
           <div className="font-montagaR400">what im capable of</div>
           <div className="tracking-tighter font-gelgooR400 ">
             <div className="flex flex-row justify-between">
-              <p className="font-extrabold">Programming Language</p>
-              <p className="font-extralight">JavaScript | Golang</p>
+              <p className="font-extrabold">C/C++</p>
+              <p className="font-extralight">JavaScript</p>
             </div>
             <div className="flex flex-row justify-between">
-              <p className="font-extrabold">Framework</p>
-              <p className="font-extralight">express.js | Gin</p>
+              <p className="font-extrabold">Computaional Thinking</p>
+              <p className="font-extralight">Basic Electronic</p>
             </div>
             <div className="flex flex-row justify-between">
-              <p className="font-extrabold">Design Pattern</p>
-              <p className="font-extralight">Repository Layer</p>
+              <p className="font-extrabold">Front-End Web Development</p>
+              <p className="font-extralight">Github</p>
             </div>
-            <div className="flex flex-row justify-between">
+            {/* <div className="flex flex-row justify-between">
               <p className="font-extrabold">Databases</p>
               <p className="font-extralight">MySQL | Firebase</p>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
